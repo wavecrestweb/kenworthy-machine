a0a77feac48ffd7f7803469ef00256b6fc1e6000
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-01-10 23:19:54 -0800

    Initial commit

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Lexend_Deca, Inter } from "next/font/google";
+
+export const lexend_deca = Lexend_Deca({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
