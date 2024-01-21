45c56181d14378a752b16d5b16ce9f82ea2ec17b
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-01-21 09:00:36 -1000

    KM-3 Fix merge conflict in providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,9 @@
 
 // LIBRARY IMPORTS
 import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from "./theme";
 
 // LOCAL IMPORTS
-import customTheme from './theme';
+import customTheme from "./theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ChakraProvider theme={customTheme}>{children}</ChakraProvider>;
