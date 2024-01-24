0b2de4f0cc735490a882d07c62682ccdf853437d
alibacova <libacova.a@gmail.com>
2024-01-24 14:27:53 -0800

    refactor(footer): Move footer content to arrays of objects and refactor rendering with map functions

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,10 +4,42 @@ import {
   Grid,
   GridItem,
   Link,
+  LinkProps,
   Stack,
   Text,
 } from "@chakra-ui/react";
 import Image from "next/image";
+
+const links = [
+  { href: "/about-us", text: "About Us" },
+  { href: "/view-machines", text: "View Machines" },
+  { href: "/careers", text: "Careers" },
+  { href: "/request-quote", text: "Contact Us" },
+  { href: "/request-quote", text: "Request a Quote" },
+];
+
+const contactInfo = [
+  {
+    heading: "Mailing Address",
+    content: [
+      "14751 North Kelsey Street, Suite 105",
+      "PMB 616",
+      "Monroe, WA 98272-1458",
+    ],
+  },
+  {
+    heading: "Shipping Address",
+    content: ["30330 NE 172nd Street", "Duvall, WA 98019"],
+  },
+  {
+    heading: "Phone",
+    content: ["(425) 788-2100"],
+  },
+  {
+    heading: "Email",
+    content: ["sales@kenworthymachine.com"],
+  },
+];
 
 const Footer = () => {
   return (
@@ -22,68 +54,40 @@ const Footer = () => {
               height={500}
             />
           </Box>
-          <Box>
-            <Text variant="footer" textDecoration="underline">
-              Mailing Address
-            </Text>
-            <Text variant="footer">14751 North Kelsey Street, Suite 105</Text>
-            <Text variant="footer">PMB 616</Text>
-            <Text variant="footer">Monroe, WA 98272-1458</Text>
-          </Box>
-
-          <Box>
-            <Text variant="footer" textDecoration="underline">
-              Shipping Address
-            </Text>
-            <Text variant="footer">30330 NE 172nd Street</Text>
-            <Text variant="footer">Duvall, WA 98019</Text>
-          </Box>
-
-          <Box>
-            <Text variant="footer" textDecoration="underline">
-              Phone
-            </Text>
-            <Text variant="footer">(425) 788-2100</Text>
-          </Box>
-
-          <Box>
-            <Text variant="footer" textDecoration="underline">
-              Email
-            </Text>
-            <Text variant="footer">sales@kenworthymachine.com</Text>
-          </Box>
+          <>
+            {contactInfo.map(({ heading, content }) => (
+              <Box key={heading}>
+                <Heading variant="footer">{heading}</Heading>
+                {content.map((line) => (
+                  <Text key={line} variant="footer">
+                    {line}
+                  </Text>
+                ))}
+              </Box>
+            ))}
+          </>
         </Stack>
       </GridItem>
       <GridItem colStart={4} colEnd={6}>
         <Stack spacing="57px" pt="28px">
-          <Box>
-            <Link href="/about-us" variant="footer">
-              About Us
-            </Link>
-          </Box>
-          <Box>
-            <Link href="/view-machines" variant="footer">
-              View Machines
-            </Link>
-          </Box>
-          <Box>
-            <Link href="/careers" variant="footer">
-              Careers
-            </Link>
-          </Box>
-          <Box>
-            <Link href="/request-quote" variant="footer">
-              Contact Us
-            </Link>
-          </Box>
-          <Box>
-            <Link href="/request-quote" variant="footer">
-              Request a Quote
-            </Link>
-          </Box>
+          {links.map(({ href, text }) => (
+            <FooterNavLink key={text} href={href}>
+              {text}
+            </FooterNavLink>
+          ))}
         </Stack>
       </GridItem>
     </Grid>
+  );
+};
+
+const FooterNavLink = ({ children, ...props }: LinkProps) => {
+  return (
+    <Box>
+      <Link variant="footer" {...props}>
+        {children}
+      </Link>
+    </Box>
   );
 };
 
