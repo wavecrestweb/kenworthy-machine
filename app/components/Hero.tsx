203ebb483bc517ec2d331579ad22270f7a0989bf
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2024-01-25 11:01:02 -0800

    feat(hero): working on matching layout and positioning to wireframes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,19 @@
 // LIBRARY IMPORTS
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 
 // LOCAL IMPORTS
-import heroImage from '../../public/images/hero-photo.png';
+import heroImage from "../../public/images/hero-photo.png";
 
 // HERO COMPONENT DEFINITION
 export default function Hero() {
-
   return (
-    <Box position="relative" textAlign="center" bg="white">
+    <Box
+      position="relative"
+      textAlign="center"
+      bg="white"
+      minH="calc(38.25vw + 6rem)"
+    >
       <Image
         src={heroImage}
         alt={"machines photo"}
@@ -19,15 +23,21 @@ export default function Hero() {
         objectFit="cover"
         objectPosition="center"
       />
-      <Box position="absolute" w="93%" top="100%" left="50%" transform="translate(-50%, -50%)">
-        <Heading
-          as="h1"
-          size="2xl"
-          color="white"
-        >
+      <Box
+        position="absolute"
+        w="93%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        height="6rem"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        opacity="90%"
+      >
+        <Heading size="xl" color="white">
           Machining Services
         </Heading>
       </Box>
     </Box>
   );
-};
+}
