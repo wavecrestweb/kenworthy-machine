5d4103c79640d4edfe227b95b1ce2f86957cd879
Archaa Work <archaaworks@Archaas-Air.lan>
2024-01-31 13:52:14 -0800

    feat(values):updates layout to better match wireframe

--- app/components/CompanyValues.tsx
+++ app/components/CompanyValues.tsx
@@ -1,7 +1,6 @@
 // LIBRARY IMPORTS
 import {
   Box,
-  Divider,
   Flex,
   Heading,
   HStack,
@@ -11,7 +10,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import Image from "next/image";
-import { FaClock, FaPeopleGroup, FaRegStar } from "react-icons/fa6";
+import { FaRegClock, FaPeopleGroup, FaRegStar } from "react-icons/fa6";
 
 // LOCAL IMPORTS
 import gearBackground from "../../public/images/machine-gear-background.png";
@@ -38,71 +37,106 @@ export default function CompanyValues() {
         bg="brand.backgroundDark"
         opacity="90%"
       >
-        <VStack spacing={8} align="stretch">
+        <VStack spacing={8}>
           <Heading color="white" textAlign="center" mb={4}>
             {"Kenworthy's Values"}
           </Heading>
-          <VStack bg="white" borderRadius="2xl" px={32} py={24} spacing={36}>
-            <Box>
-              <HStack spacing={4} align="left">
-                <VStack align="start">
-                  <Heading size="md">Quality</Heading>
-                  <Divider color="brand.button" />
-                  <Text>
-                    <p>
-                      {
-                        "At Kenworthy Machine, producing quality parts to our customer's specifications is our most important job."
-                      }
-                    </p>
-                    <p>
-                      {
-                        "Our team is dedicated to making sure your parts are correct an in tolerance every time. They use their expertise, as well as modern inspection equipment to achieve this goal. Upon request we can provide inspection reports for your parts with detailed measurements compared to drawing specifications. We also offer laser engraving to give your parts a professional appearance."
-                      }
-                    </p>
-                  </Text>
-                </VStack>
-                <Icon as={FaRegStar} fontSize="3xl" />
-              </HStack>
-            </Box>
-            <Box>
-              <HStack spacing={4} align="right">
-                <Icon as={FaClock} fontSize="3xl" />
-                <VStack align="start">
-                  <Heading size="md">On Time Delivery</Heading>
-                  <Divider color="brand.button" />
-                  <Text>
-                    <p>
-                      We strive to be a supplier that our customers can rely on.
-                    </p>
-                    <p>
-                      We use a modern ERP system to accurately schedule our shop
-                      so that we give our customers accurate delivery date
-                      commitments.
-                    </p>
-                  </Text>
-                </VStack>
-              </HStack>
-            </Box>
-            <Box>
-              <HStack spacing={4} align="left">
-                <VStack align="start">
-                  <Heading size="md">Collaboration</Heading>
-                  <Divider color="brand.button" />
-                  <Text>
-                    <p>We value our relationships with our customers.</p>
-                    <p>
-                      It is important that we partner with them to give the best
-                      possible value. Let us work with your engineering and
-                      design teams to maximize value. Often our expert machining
-                      team is able to provide feedback from a manufacturing
-                      perspective that reduces costs while retaining
-                      functionality.
-                    </p>
-                  </Text>
-                </VStack>
-                <Icon as={FaPeopleGroup} fontSize="3xl" />
-              </HStack>
-            </Box>
+          <VStack
+            maxW="1400px"
+            bg="white"
+            borderRadius="2xl"
+            px={32}
+            py={24}
+            spacing={36}
+          >
+            <HStack spacing={12} align="center">
+              <VStack align="start" spacing={4}>
+                <Heading size="lg">Quality</Heading>
+                <Box
+                  bgGradient="linear(90deg, #DB0A40 0%, rgba(219, 10, 64, 0.04) 96%, rgba(219, 10, 64, 0) 100%)"
+                  w="full"
+                  h={1}
+                ></Box>
+                <Text fontWeight="bold" fontSize="lg">
+                  {
+                    "At Kenworthy Machine, producing quality parts to our customer's specifications is our most important job."
+                  }
+                </Text>
+                <Text fontSize="lg">
+                  {
+                    "Our team is dedicated to making sure your parts are correct an in tolerance every time. They use their expertise, as well as modern inspection equipment to achieve this goal. Upon request we can provide inspection reports for your parts with detailed measurements compared to drawing specifications. We also offer laser engraving to give your parts a professional appearance."
+                  }
+                </Text>
+              </VStack>
+              <Spacer />
+              <Flex
+                w={28}
+                h={28}
+                bg="brand.backgroundDark"
+                borderRadius="2xl"
+                align="center"
+              >
+                <Icon as={FaRegStar} w={24} h={24} color="white" />
+              </Flex>
+            </HStack>
+            <HStack spacing={12} align="right">
+              <Flex
+                w={32}
+                h={32}
+                bg="brand.backgroundDark"
+                borderRadius="2xl"
+                align="center"
+                justify="center"
+              >
+                <Icon as={FaRegClock} w={28} h={28} color="white" />
+              </Flex>
+              <Spacer />
+              <VStack align="start" spacing={4}>
+                <Heading size="lg">On Time Delivery</Heading>
+                <Box
+                  bgGradient="linear(90deg, #DB0A40 0%, rgba(219, 10, 64, 0.04) 96%, rgba(219, 10, 64, 0) 100%)"
+                  w="full"
+                  h={1}
+                ></Box>
+                <Text fontWeight="bold" fontSize="lg">
+                  We strive to be a supplier that our customers can rely on.
+                </Text>
+                <Text fontSize="lg">
+                  We use a modern ERP system to accurately schedule our shop so
+                  that we give our customers accurate delivery date commitments.
+                </Text>
+              </VStack>
+            </HStack>
+            <HStack spacing={12} align="left">
+              <VStack align="start" spacing={4}>
+                <Heading size="lg">Collaboration</Heading>
+                <Box
+                  bgGradient="linear(90deg, #DB0A40 0%, rgba(219, 10, 64, 0.04) 96%, rgba(219, 10, 64, 0) 100%)"
+                  w="full"
+                  h={1}
+                ></Box>
+                <Text fontWeight="bold" fontSize="lg">
+                  We value our relationships with our customers.
+                </Text>
+                <Text fontSize="lg">
+                  It is important that we partner with them to give the best
+                  possible value. Let us work with your engineering and design
+                  teams to maximize value. Often our expert machining team is
+                  able to provide feedback from a manufacturing perspective that
+                  reduces costs while retaining functionality.
+                </Text>
+              </VStack>
+              <Spacer />
+              <Flex
+                w={32}
+                h={32}
+                bg="brand.backgroundDark"
+                borderRadius="2xl"
+                align="center"
+              >
+                <Icon as={FaPeopleGroup} w={28} h={28} color="white" />
+              </Flex>
+            </HStack>
           </VStack>
         </VStack>
       </Box>
