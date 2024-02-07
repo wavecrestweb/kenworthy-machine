57872a9c9e3779b3601b5310c29bb7a9aee6a7b7
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-02-06 18:10:48 -0800

    feat(carousel): Install react-slick, slick-carousel, and types/react-slick. Create CardsSlider component using Slider from react-slick, with basic configuration, returning my example card data. Create Machines component as the container of the CardsSlider. Add Machines to view-machines page. Add dummy data to CardsSlider for testing.

--- app/(routes)/view-machines/page.tsx
+++ app/(routes)/view-machines/page.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
+import Machines from "@/app/components/Machines";
 
 export default function ViewMachines() {
   return (
     <Box>
       <h1>This is the View Machines page.</h1>
+      <Machines />
     </Box>
   );
 }
