3679ffa5cb4c0a10d8d98e6bdde1aa41e4002829
alibacova <libacova.a@gmail.com>
2024-02-08 21:39:13 -0800

    feat(mahine carousel): Pass data from TinaCMS

--- app/components/MachineCarousel.tsx
+++ app/components/MachineCarousel.tsx
@@ -2,24 +2,27 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 import { Link as ChakraLink } from "@chakra-ui/next-js";
 import ContentWrapper from "./ContentWrapper";
 import Card from "./Card";
+import { PageBlocksMachineCarousel } from "@/tina/__generated__/types";
 
-export default function MachineCarousel() {
+export default function MachineCarousel(props: PageBlocksMachineCarousel) {
   return (
     <Box>
       <ContentWrapper>
         <Flex py="24" direction="column" alignItems="center">
           <Heading as="h2" textStyle="h1" pb="8">
-            Our Machines
+            {props.sectionTitle}
           </Heading>
           <Box pb="20">
-            <Card></Card>
+            {props.machineCard?.map((card, i) => {
+              return <Card key={i} {...card}></Card>;
+            })}
           </Box>
           <ChakraLink
             href="/view-machines"
             variant="primaryWhiteBg"
             size={{ base: "sm", sm: "xl" }}
           >
-            View Machines
+            {props.buttonLabel}
           </ChakraLink>
         </Flex>
       </ContentWrapper>
