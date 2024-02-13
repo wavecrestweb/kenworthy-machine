d993aa17d929df78658ac7bb17ff8df4fbbc54ff
alibacova <libacova.a@gmail.com>
2024-02-13 15:08:51 -0800

    fix(machines section): Adjust sizing to get a closer match with wireframes

--- app/components/Machines.tsx
+++ app/components/Machines.tsx
@@ -1,11 +1,12 @@
 import { Link as ChakraLink } from "@chakra-ui/next-js";
-import { Container, Heading, Box } from "@chakra-ui/react";
+import { Container, Heading, Box, Flex } from "@chakra-ui/react";
 import { PageQuery } from "@/tina/__generated__/types";
 
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 import MachineCarousel from "./MachineCarousel";
+import ContentWrapper from "./ContentWrapper";
 
 type Block = NonNullable<NonNullable<PageQuery["page"]["blocks"]>[number]>;
 
@@ -13,29 +14,39 @@ export type MachineCardsBlock = Extract<
   Block,
   { __typename: "PageBlocksMachineCarousel" }
 >;
+
 export default function Machines({
   width,
   ...props
 }: { width: number } & MachineCardsBlock): JSX.Element {
-  const adjustedWidth = width * 0.9;
+  const adjustedWidth = width <= 1728 ? width * 0.9 : 1728 * 0.9;
 
   return (
-    <Box maxW={width} backgroundColor="white">
-      <Container maxW={adjustedWidth} bg="white" py="4rem">
-        <Heading as="h2" textStyle="h1" textAlign="center" mb={4} pb={2}>
-          {props.sectionTitle}
-        </Heading>
-        <MachineCarousel machineCards={props?.machineCards} />
-        <Container maxW={"250px"} mt={20}>
-          <ChakraLink
-            href="/view-machines"
-            variant="buttonPrimaryLight"
-            size={{ base: "sm", sm: "xl" }}
+    <Flex justifyContent="center">
+      <ContentWrapper>
+        <Container maxW={adjustedWidth} bg="white" py="4rem" px="0px" mx={8}>
+          <Heading
+            as="h2"
+            textStyle="h1"
+            fontWeight="normal"
+            textAlign="center"
+            mb={4}
+            pb={2}
           >
-            {props.buttonLabel}
-          </ChakraLink>
+            {props.sectionTitle}
+          </Heading>
+          <MachineCarousel machineCards={props?.machineCards} />
+          <Flex mt={20} mb={8} justifyContent="center">
+            <ChakraLink
+              href="/view-machines"
+              variant="buttonPrimaryLight"
+              size={{ base: "sm", sm: "xl" }}
+            >
+              {props.buttonLabel}
+            </ChakraLink>
+          </Flex>
         </Container>
-      </Container>
-    </Box>
+      </ContentWrapper>
+    </Flex>
   );
 }
