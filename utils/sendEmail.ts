32d3843bbfbcd13657c3d448950ce1d0a24941fa
Archaa Work <archaaworks@Archaas-Air.lan>
2024-02-20 15:41:19 -0800

    fix(form submit): adds extra layer or error catching

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -10,7 +10,13 @@ export function sendEmail(data: FormData) {
     },
     body: JSON.stringify(data),
   })
-    .then((res) => res.json())
+    .then((res) => {
+      console.log("response passed from fetch: ", res);
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
     .then((response) => {
       alert(response.message);
       console.log("response: ", response);
