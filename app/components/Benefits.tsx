f27981dc3ae7cc80285940348ed8a4222b5ee1a1
alibacova <libacova.a@gmail.com>
2024-02-27 12:51:14 -0800

    feat(benefits section): Style all the elements to match the wireframes

--- app/components/Benefits.tsx
+++ app/components/Benefits.tsx
@@ -1,10 +1,10 @@
 //LIBRARY IMPORTS
-import { Box, Heading, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, Grid, GridItem, Flex, Card } from "@chakra-ui/react";
+import Image from "next/image";
 import { PageQuery } from "@/tina/__generated__/types";
 
 //LOCAL IMPORTS
 import ContentWrapper from "./ContentWrapper";
-import Image from "next/image";
 
 type Block = NonNullable<NonNullable<PageQuery["page"]["blocks"]>>[number];
 type Benefits = NonNullable<
@@ -15,26 +15,63 @@ export default function Benefits(props: Benefits): JSX.Element {
   return (
     <Box>
       <ContentWrapper>
-        <Heading>{props?.heading}</Heading>
-        <SimpleGrid columns={{ base: 1, xl: 3 }}>
-          <Box>
-            {props.benefits && (
-              <>
-                {props.benefits.map((benefit, i) => (
-                  <>
-                    <Image
-                      src={benefit?.icon || ""}
-                      alt=""
-                      width="100"
-                      height="100"
-                    />
-                    <Heading>{benefit?.benefitTitle}</Heading>
-                  </>
-                ))}
-              </>
-            )}
-          </Box>
-        </SimpleGrid>
+        <Flex direction="column" alignItems="center">
+          <Heading
+            as="h2"
+            fontSize={{ base: "xl", sm: "3xl", xl: "4xl" }}
+            lineHeight={{ base: "6", sm: "2.7rem", xl: "3rem" }}
+            fontWeight="normal"
+            layerStyle="whiteBg"
+            py={{ base: "20", xl: "28" }}
+          >
+            {props?.heading}
+          </Heading>
+          {props.benefits && (
+            <Grid
+              templateColumns={{ base: "1", xl: "repeat(3, 1fr)" }}
+              gap={{ base: "16", xl: "28" }}
+              pb={{ base: "20", xl: "36" }}
+            >
+              {props.benefits.map((benefit, i) => (
+                <GridItem key={i}>
+                  <Flex
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap="8"
+                  >
+                    <Card
+                      w="8.75rem"
+                      h="8.75rem"
+                      align="center"
+                      position="relative"
+                    >
+                      <Image
+                        src={benefit?.icon || ""}
+                        alt=""
+                        fill={true}
+                        style={{
+                          objectFit: "contain",
+                          padding: "1rem",
+                        }}
+                      />
+                    </Card>
+                    <Heading
+                      as="h4"
+                      fontSize={{ base: "lg", sm: "2xl" }}
+                      lineHeight={{ base: "7", sm: "8" }}
+                      fontWeight="bold"
+                      textAlign="center"
+                      layerStyle="whiteBg"
+                    >
+                      {benefit?.benefitTitle}
+                    </Heading>
+                  </Flex>
+                </GridItem>
+              ))}
+            </Grid>
+          )}
+        </Flex>
       </ContentWrapper>
     </Box>
   );
