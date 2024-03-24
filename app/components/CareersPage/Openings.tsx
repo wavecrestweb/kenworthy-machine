af4c66e9bc0986a8c333b89bc59a2c6b889c2930
alibacova <libacova.a@gmail.com>
2024-03-24 16:56:31 -0700

    feat(openings section): Set up skeleton and implement opening prepopulated default email client

--- app/components/CareersPage/Openings.tsx
+++ app/components/CareersPage/Openings.tsx
@@ -1,6 +1,5 @@
 //LIBRARY IMPORTS
-import { Box, Heading, Grid, GridItem, Flex, Card } from "@chakra-ui/react";
-import Image from "next/image";
+import { Box, Heading, Flex, Card, Text, Link } from "@chakra-ui/react";
 import { PageQuery } from "@/tina/__generated__/types";
 
 //LOCAL IMPORTS
@@ -11,11 +10,41 @@ type OpeningsProps = NonNullable<
   Extract<Block, { __typename: "PageCareersBlocksOpenings" }>
 >;
 
+const subjectLine = "[Job title]%20Resume %20for%20future%20openings%20";
+const emailLink = `mailto:${process.env.NEXT_PUBLIC_EMAIL_TO}?subject=${subjectLine}`;
+
 export default function Openings(props: OpeningsProps): JSX.Element {
+  function OpeningsAvailable() {
+    return <div>Openings</div>;
+  }
+
+  function NoOpenings() {
+    return (
+      <>
+        <Text textAlign="center">{props.noOpenings?.description}</Text>
+        <Link href={emailLink} isExternal target="_blank" color="brand.text">
+          {props.noOpenings?.buttonText}
+        </Link>
+      </>
+    );
+  }
   console.log("openings", props);
   return (
     <Box>
-      <ContentWrapper>Openings</ContentWrapper>
+      <ContentWrapper>
+        <Flex direction="column" alignItems="center">
+          <Heading>
+            {props.isOpeningsListNotEmpty
+              ? props.openingsList?.heading
+              : props.noOpenings?.heading}
+          </Heading>
+          {props?.isOpeningsListNotEmpty ? (
+            <OpeningsAvailable />
+          ) : (
+            <NoOpenings />
+          )}
+        </Flex>
+      </ContentWrapper>
     </Box>
   );
 }
