3c492a62a56d2d2d4150e0ba3fd222fbd076185d
alibacova <libacova.a@gmail.com>
2024-03-25 14:30:43 -0700

    fix(opening card): Refactor to use original Figma sizing; add content wrapper; fix link size

--- app/components/CareersPage/Openings.tsx
+++ app/components/CareersPage/Openings.tsx
@@ -30,35 +30,47 @@ export default function Openings(props: OpeningsProps): JSX.Element {
         {props.openingsList?.positions?.map((position, i) => (
           <Card
             key={i}
-            variant={{ base: "position_sm", "2xl": "position_md" }}
-            w={{ base: "95vw", sm: "88vw", "2xl": "95vw" }}
+            variant={{
+              base: "position_sm",
+              "2xl": "position_lg",
+            }}
+            maxW={{ base: "95vw", md: "88vw", "2xl": "95vw" }}
             flexDirection={{ base: "column", sm: "row" }}
           >
             <CardHeader>
               <Heading
                 color="brand.text"
-                fontSize={{ base: "lg", sm: "xl", "2xl": "3xl" }}
-                fontWeight={{ "2xl": "normal" }}
-                lineHeight={{ base: "5", sm: "6", "2xl": "10" }}
+                fontSize={{ base: "2xl", sm: "2rem", "2xl": "5xl" }}
+                fontWeight={{ base: "bold", xl: "normal" }}
+                lineHeight={{ base: "7", sm: "10", "2xl": "3.6rem" }}
               >
                 {position?.title}
               </Heading>
               <Text
                 color="brand.text"
-                fontSize={{ base: "md", sm: "lg", "2xl": "xl" }}
-                lineHeight={{ base: "5", sm: "5", "2xl": "6" }}
+                fontSize={{ base: "xl", sm: "2xl", "2xl": "2rem" }}
+                lineHeight={{ base: "6", sm: "7", "2xl": "10" }}
                 pt={{ base: "1.25rem", "2xl": "2rem" }}
+                pr={{ sm: "1rem" }}
               >
                 {position?.subtitle}
               </Text>
             </CardHeader>
             <CardBody></CardBody>
-            <CardFooter p={{ base: "2.5rem 0 0 0", sm: "0" }}>
+            <CardFooter
+              p={{ base: "2.5rem 0 0 0", sm: "0" }}
+              minW={{ base: "0%", md: "max-content" }}
+              flex={{ base: "1 0 auto" }}
+            >
               <Link
-                color="brand.text"
                 href={position?.jobDescriptionLink || ""}
                 isExternal
                 target="_blank"
+                variant="outline"
+                fontSize={{ base: "xl", md: "2xl", "2xl": "2xl" }}
+                lineHeight={{ base: "6", md: "7" }}
+                minW={{ base: "85vw", sm: "100%" }}
+                maxW={{ base: "0%" }}
               >
                 {props.openingsList?.buttonText}
               </Link>
@@ -81,21 +93,15 @@ export default function Openings(props: OpeningsProps): JSX.Element {
   }
 
   return (
-    <Box>
-      <ContentWrapper>
-        <Flex direction="column" alignItems="center">
-          <Heading>
-            {props.isOpeningsListNotEmpty
-              ? props.openingsList?.heading
-              : props.noOpenings?.heading}
-          </Heading>
-          {props?.isOpeningsListNotEmpty ? (
-            <OpeningsAvailable />
-          ) : (
-            <NoOpenings />
-          )}
-        </Flex>
-      </ContentWrapper>
-    </Box>
+    <ContentWrapper>
+      <Flex direction="column" alignItems="center">
+        <Heading>
+          {props.isOpeningsListNotEmpty
+            ? props.openingsList?.heading
+            : props.noOpenings?.heading}
+        </Heading>
+        {props?.isOpeningsListNotEmpty ? <OpeningsAvailable /> : <NoOpenings />}
+      </Flex>
+    </ContentWrapper>
   );
 }
