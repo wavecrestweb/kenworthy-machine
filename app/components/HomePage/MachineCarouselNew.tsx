ad553a5ad3611585fc10caa7ab20eb729b3cdf2b
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-04-22 16:53:12 -0700

    chore(KM-46): In Machines, change Container to Box and add responsive maxW sizes. Rewrite breakpoints for Swiper in MachineCarouselNew.

--- app/components/HomePage/MachineCarouselNew.tsx
+++ app/components/HomePage/MachineCarouselNew.tsx
@@ -82,8 +82,6 @@ export default function MachineCarouselNew({
 }): JSX.Element {
   return (
     <Swiper
-      // slidesPerView={3}
-      // autoHeight
       navigation
       pagination={{ clickable: true }}
       // modules={[Navigation, Pagination, A11y]}
@@ -92,36 +90,26 @@ export default function MachineCarouselNew({
       modules={[FreeMode, Navigation, Pagination, A11y]}
       className="mySwiper"
       breakpoints={{
-        // when window width is >= 320px
-        320: {
+        // when window width is >= 640px
+        640: {
           slidesPerView: 1,
           spaceBetween: 10,
         },
-        // when window width is >= 480px
-        480: {
-          slidesPerView: 1,
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
           spaceBetween: 20,
         },
-        // when window width is >= 640px
-        640: {
+        // when window width is >= 1024px
+        1024: {
           slidesPerView: 3,
           spaceBetween: 30,
         },
-        // when window width is >= 768px
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        // when window width is >= 1024px
-        // 1024: {
-        //   slidesPerView: 5,
-        //   spaceBetween: 50,
-        // },
       }}
     >
       {machineCards?.machineCard?.map((card, i) => {
         return (
-          <SwiperSlide key={i} color={"purple"}>
+          <SwiperSlide key={i}>
             <Card key={i} {...card} path={path} />
             <Spacer padding={8} />
           </SwiperSlide>
