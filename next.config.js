2731435b315bbc02ca0f2eb95136b25dc8bcaf96
alibacova <libacova.a@gmail.com>
2024-07-18 18:18:02 -0700

    fix(external images): Allow access to external images hosted on the Tina media hostname

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+        port: "",
+        pathname: `/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/**`,
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
